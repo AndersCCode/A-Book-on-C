005b6d565bcaef5a85180ca23aa080ffd3a761f5
Anders Nyqvist <anders.nyqvist@kau.se>
2026-02-16 15:13:11 +0100

    3.1

---
+++ Chapter_3/3_1/main2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define MAX 10
+
+int binsearch(int x, int v[], int n) {
+    int low, mid, high; // index
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low+high) / 2;
+        
+        if (x < v[mid]) 
+            high = mid -1;
+        else 
+            low = mid + 1;
+    } 
+
+    if (low < n && v[low] == x)
+     return -1; // No match
+}
+
+int main(void) {
+
+    int v[MAX];
+    int x = 77;
+    int val = 0;
+
+    for (int i = 0; i < MAX; i++) {
+        v[i] = i + 2;
+        printf("v[%d] = %d\n", i, v[i]);
+    }
+
+    // printf("Position %d\n", binsearch(x, v, MAX));
+    val = binsearch(x, v, MAX);
+    (val > -1) ? printf("%d is on position %d\n", x, val) : printf("%d not found\n", x);
+    return 0;
+}
