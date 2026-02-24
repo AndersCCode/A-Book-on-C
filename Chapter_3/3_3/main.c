d303582fa41e8865b16a04da902525cc6d78e453
Anders Nyqvist <anders.nyqvist@kau.se>
2026-02-24 14:14:49 +0100

    3.3 completed

--- Chapter_3/3_3/main.c
+++ Chapter_3/3_3/main.c
@@ -5,19 +5,63 @@ like a-b-c and a-z0-9 and -a-z. Arrange that a leading or trailing - is
 taken literally.*/
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-void expand(char s1[], char s2[]) {
-    
+void expand(const char s1[], char s2[]) {
+    int i = 0, j = 0;
+
+    while (s1[i] != '\0') {
+        // Is this a - followed by at least one character?
+        if (s1[i+1] == '-' && s1[i+2] != '\0') {
+            char start = s1[i];
+            char end = s1[i+2];
+
+            if (start <= end && 
+                ((islower(start) && islower(end)) ||
+                (isupper(start) && isupper(end)) ||
+                (isdigit(start) && isdigit(end)))) {
+                
+                for (char c = start; c <= end; c++) {
+                        s2[j++] = c;
+                }
+                i += 2; // skip over the '-' and the end char
+            }
+            else {
+                s2[j++] = s1[i]; // Invalid range i.e. z-a, A-z, 1-3
+            }
+        }
+        else {
+            s2[j++] = s1[i]; // Invalid syntax. Just copy the character.
+        }
+        i++;
+    }
+    s2[j] = '\0';
 }
 
 int main(void) {
 
-    char source[] = "a-z";
-    char destination[] = 200;
+    char tests[][50] = {
+        "a-c", // abc
+         "-a-z", // abcdefghijklmnopqrstuvxyz
+         "a-z-", // abcdefghijklmnopqrstuvxyz-
+         "a-b-c", // ab-c
+         "0-9", // 0123456789
+         "a-z0-9", //abcdefghijklmnopqrstuvxyz0123456789
+         "z-a", // z-a
+         "A-Z", // ABCDEFGHIJKLMNOPQRSTUVXYZ
+         "a--b" // a--b
+    };
+
+    int num_tests = sizeof(tests) / sizeof(tests[0]);
     
-    expand(source, destination);
+    char result[200];
 
-    printf("Source = %s\n Destination = %s\n", source, destination);
+    for (int t = 0; t < num_tests; t++) {
+        
+        expand(tests[t], result);
 
+        printf("'%s' --> '%s'\n", tests[t], result);
+    }
     return 0;
 }
