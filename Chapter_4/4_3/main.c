43728f795d85c6ff7f955b7566dbb1a820f44d7b
Anders Nyqvist <anders.nyqvist@kau.se>
2026-03-03 14:37:56 +0100

    4.3

--- Chapter_4/4_3/main.c
+++ Chapter_4/4_3/main.c
@@ -1,3 +1,6 @@
+/* Given the basic framework, it's straightforward to extend the calculator. Add the modulus (%) operator 
+and provisions for negative numbers */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
