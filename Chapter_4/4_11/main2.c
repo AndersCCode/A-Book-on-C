ba1105d2fb310222e78d3f31e8be83559881c5d0
Anders Nyqvist <anders.nyqvist@kau.se>
2026-03-24 14:58:54 +0100

    4.11 optimization

--- Chapter_4/4_11/main2.c
+++ Chapter_4/4_11/main2.c
@@ -281,16 +281,6 @@ int main(void) {
                 printf("Stack cleared\n");
                 break;
 
-            case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I':
-            case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R':
-            case 'S': case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z': {
-
-                int var = type - 'A';
-
-                push(variables[var]);
-                break; 
-            }
-
             default:
                 printf("error: unknown command %c\n", type);
                 break;            
