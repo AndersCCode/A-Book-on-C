815e2ee4b3e440f78e2b68c685786ae6d2204516
Anders Nyqvist <andersnyqvist420@gmail.com>
2026-03-31 19:41:37 +0200

    4.14

---
+++ Chapter_4/4_14/main.c
@@ -0,0 +1,15 @@
+/* Define a macro swap(t,x,y) that interchanges two arguments of type t.
+Block structure will help */
+
+#include <stdio.h>
+
+void swap(t, x, y) {
+
+}
+
+int main(void) {
+    
+    swap(t,x,y);
+    
+    return 0;
+}
