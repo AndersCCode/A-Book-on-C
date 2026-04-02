20b1df0608cb9460d32f65841ff6a45fe5cd8d61
Anders Nyqvist <anders.nyqvist@kau.se>
2026-04-02 13:57:40 +0200

    4.13 almost done

---
+++ Chapter_4/4_14/bad.c
@@ -0,0 +1,22 @@
+/* Define a macro swap(t,x,y) that interchanges two arguments of type t.
+Block structure will help */
+
+#include <stdio.h>
+
+/* _temp to reduce risk of name clash with user variables */
+//#define swap(t, x, y) do { t _temp = (x); (x) = (y); (y) = _temp; } while (0) 
+#define swap(t, x, y) { t _temp = (x); (x) = (y); (y) = _temp; } 
+
+int main(void) {
+    
+    int a = 10, b = 20;
+
+    if (a < b)
+        swap(int, a, b);
+    else
+        printf("No swap\n");
+
+    printf("After: a=%d b=%d\n", a, b);
+    
+    return 0;
+}
--- Chapter_4/4_14/main.c
+++ Chapter_4/4_14/main.c
@@ -3,15 +3,27 @@ Block structure will help */
 
 #include <stdio.h>
 
-void swap(t, x, y) {
+/* _temp to reduce risk of name clash with user variables
+do while executes only one time (false on first evaluation)
+() protects against side effects and operator precedence problems
+ and also improve readability and clarity */
 
-}
+
+#define swap(t, x, y) do { t _temp = (x); (x) = (y); (y) = _temp; } while (0) 
 
 int main(void) {
     
-    swap(t,x,y);
+    int a = 10, b = 20;
 
-    // Let's go!
-    
+    printf("Before: a=%d b=%d\n", a, b);
+    swap(int, a, b);
+    printf("After: a=%d b=%d\n", a, b);
+
+    double x = 1.5, y = 3.7;
+
+    printf("Before: x=%f y=%f\n", x, y);
+    swap(float, x, y);
+    printf("After: x=%f y=%f\n", x, y);
+
     return 0;
 }
