4f122a656f3a97c712503cac07e9487b717e1be9
Anders Nyqvist <anders.nyqvist@kau.se>
2026-04-08 15:03:06 +0200

    5.3 completed

---
+++ Chapter_5/5_3/main.c
@@ -0,0 +1,34 @@
+/* Write a pointer version of the function strcat that we showed in Chapter 2: strcat(s,t) copies the string t 
+to the end of s */
+
+/* Concatenate t to the end of s; s must be big enough */
+
+#include <stdio.h>
+
+#define MAXLEN 100
+
+void mystrcat(char *s, char *t) {
+
+    /* find end of s */
+    while (*s != '\0')                
+        s++;
+    
+    /* append t */
+    while ((*s++ = *t++) != '\0')   
+        ;
+}
+
+int main(void) {
+    
+    char original[MAXLEN] = "abc";
+    char addendum[MAXLEN] = "def";
+    
+    printf("Original string: %s\n", original);
+
+    mystrcat(original, addendum);
+
+    printf("Final string: %s\n", original);
+
+    return 0;
+}
+
