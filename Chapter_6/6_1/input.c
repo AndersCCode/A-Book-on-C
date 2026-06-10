4aa523453d4464d3e5b5afecf0f1c08d995f7fce
Anders Nyqvist <anders.nyqvist@kau.se>
2026-06-10 14:49:41 +0200

    6.1 continued

--- Chapter_6/6_1/input.c
+++ Chapter_6/6_1/input.c
@@ -1,5 +1,15 @@
 int main(void)
 {
+    /*  without underscore */
+    int count = 0;
+    char quote = '"'; // "
+    
+    /* identifier with underscore is not a keyword (belonging to the C language) */
+    int my_int = 0;
+    int _leading = 1;
+    int trailing_ = 2;
+    int a_b_c = 3;
+
     return 0;
 }
 
--- Chapter_6/6_1/main.c
+++ Chapter_6/6_1/main.c
@@ -1,4 +1,4 @@
-/* Our version of getword doesn not properly handle underscores, string constants, comments, 
+/* Our version of get_word doesn not properly handle underscores, string constants, comments, 
 or preprocessor control lines. Write a better version
 
 gcc -Wall -Wextra -O2 -o keyword_count main.c
@@ -11,38 +11,54 @@ gcc -Wall -Wextra -O2 -o keyword_count main.c
 
 #define MAXWORD 100
 #define BUFSIZE 100
-#define NKEYS (sizeof keytab / sizeof(keytab[0]))
+#define NKEYS (sizeof key_table / sizeof(key_table[0]))
 
 struct key {
     char *word;
     int count;
 };
 
-struct key keytab[] = {
-    {"/* ... */", 0},
-    {"__FILE__", 0},
-    {"__abc__", 0},
-    {"_temp", 0},
-    {"auto", 0},
-    {"break", 0},
-    {"case", 0},
-    {"char", 0},
-    {"const", 0},
-    {"continue", 0},
-    {"default", 0},
-    {"my_var", 0},
-    {"unsigned", 0},
-    {"var_123_test", 0},
-    {"void", 0},
-    {"volatile", 0},
-    {"while", 0}
+struct key key_table[] = {
+    {"auto", 0}, {"break", 0}, {"case", 0}, {"char", 0},
+    {"const", 0}, {"continue", 0}, {"default", 0}, {"do", 0},
+    {"double", 0}, {"else", 0}, {"enum", 0}, {"extern", 0},
+    {"float", 0}, {"for", 0}, {"goto", 0}, {"if", 0},
+    {"int", 0}, {"long", 0}, {"register", 0}, {"return", 0},
+    {"short", 0}, {"signed", 0}, {"sizeof", 0}, {"static", 0},
+    {"struct", 0}, {"switch", 0}, {"typedef", 0}, {"union", 0},
+    {"unsigned", 0}, {"void", 0}, {"volatile", 0}, {"while", 0}
 };
 
-char buf[BUFSIZE];      // Buffer for ungetch
-int bufp = 0;           // Next free position in buf
+char buf[BUFSIZE];      /* Buffer for ungetch */
+int bufp = 0;           /* Next free position in buf */
 
-// Find word in tab[0]...tab[n-1]
-int binsearch(char *word, struct key tab[], int n)
+static int at_beginning_of_line = 1;
+
+int binary_search(char *word, struct key tab[], int n);
+int getch(void);
+void ungetch(int c);
+int get_word(char *word, int lim);
+void skip_string(int quote);
+
+int main(void) 
+{
+    int n;
+    char word[MAXWORD];
+
+    while (get_word(word, MAXWORD) != EOF)
+        if (isalpha(word[0]) || (word[0] == '_'))
+            if ((n = binary_search(word, key_table, NKEYS)) >= 0)
+                key_table[n].count++;
+    
+    for (n = 0; n < (int)NKEYS; n++) 
+        if (key_table[n].count > 0)
+            printf("%4d %s\n", key_table[n].count, key_table[n].word);
+
+    return 0;
+}
+
+/* Find word in tab[0]...tab[n-1] */
+int binary_search(char *word, struct key tab[], int n)
 {
     int cond;
     int low, high, mid;
@@ -62,22 +78,7 @@ int binsearch(char *word, struct key tab[], int n)
     return -1;
 }
 
-// Get a (possibly pushed back) character
-int getch(void)
-{
-    return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-// Push character back on input
-void ungetch(int c)
-{
-    if (bufp >= BUFSIZE)
-        printf("ungetch: too many charachters\n");
-    else 
-        buf[bufp++] = c;
-}
-
-int getword(char *word, int lim)
+int get_word(char *word, int lim)
 {
     int c;
     char *w = word;
@@ -94,6 +95,18 @@ int getword(char *word, int lim)
     if (!isalpha(c) && (c != '_')) {    // c is not a letter and not and underscore
         *w = '\0';
         return c;
+    }
+
+    /* string literal */
+    if (c == '"') {
+        skip_string('"');
+        return get_word(word, lim);
+    }
+
+     /* character constant */
+    if (c == '\'') {
+        skip_string('\'');
+        return get_word(word, lim);
     }
 
     // Now read the rest of the identifier (letters, digits or underscore)
@@ -113,20 +126,43 @@ int getword(char *word, int lim)
     return word[0];
 }
 
-// Count C keywords
-int main(void) 
+/* Consume every character up to and including the matching closing quote. 
+get_word has already read the opening quote. */
+void skip_string(int quote)
 {
-    int n;
-    char word[MAXWORD];
+    int c;
 
-    while (getword(word, MAXWORD) != EOF)
-        if (isalpha(word[0]) || (word[0] == '_'))
-            if ((n = binsearch(word, keytab, NKEYS)) >= 0)
-                keytab[n].count++;
-    
-    for (n = 0; n < (int)NKEYS; n++) 
-        if (keytab[n].count > 0)
-            printf("%4d %s\n", keytab[n].count, keytab[n].word);
+    while ((c = getch()) != EOF) {
+        /*  If a string (or character constant) spans lines, we must correctly maintain the flag for marking 
+        beginning of the line so that a # on a later line is still properly recognized as a preprocessor directive. */
+        if (c == '\n') 
+            at_beginning_of_line = 1;
 
-    return 0;
+        if (c =='\\') {
+            /* Consume the escaped character (even if quote or backslash) */
+            int esc = getch();
+
+            if (esc == '\n')
+                at_beginning_of_line = 1;
+            continue;
+        }
+        if (c == quote)     // Found the closing quote, which was the goal.
+            return;
+    }
 }
+
+/* Get a (possibly pushed back) character */
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* Push character back on input */
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many charachters\n");
+    else 
+        buf[bufp++] = c;
+}
+
